7c2a68c7b5687362e47a6436fb003925acb2facf
Bipol Alam <bipolalam@gmail.com>
2015-07-23 20:30:19 -0500

    initial dependencies, and scaffolded out an angular node app

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+
+//static
+app.use(express.static(__dirname + '/src/'));    
+
+//routes
+app.get('*', function(req, res) {
+	res.sendFile('./src/index.html');
+});
+
+
+//create server
+var server = app.listen(8000, function () {
+	var host = server.address().address;
+	var port = server.address().port;
+
+	console.log('Example app listening at http://%s:%s', host, port);
+});
