fc2ea40bbdf84d567d1f39acd0248e0f9119fb95
Bipol Alam <bipolalam@gmail.com>
2016-01-17 10:36:48 -0600

    cleaning up angular code

--- src/app.js
+++ src/app.js
@@ -3,61 +3,66 @@ angular.module('app', [
   'ui.router'
 ]);
 
+angular.module('app').controller('searchCtrl', [ '$scope', 'search', 'socket',
+function($scope, search, socket) {
 
-angular.module('app').controller('searchCtrl', [ '$scope', 'search', 'socket', function($scope, search, socket) {
+  $scope.name = 'username'; //default value for the form
+  $scope.hits = [];
+  $scope.searching = false;
+  $scope.submit = submit;
+  socket.on('connect', onConnect);
 
-    $scope.name = 'username'; //default value for the form
-    $scope.hits = [];
-    $scope.searching = false;
+  function submit(name) {
+  socket.emit('usernames', {'name': name});
+    $scope.searching = true;
+  }
 
-    $scope.submit = function(name) {
-      socket.emit('usernames', {'name': name});
-      $scope.searching = true;
-    };
-
-    socket.on('connect', function() {
-      console.log('on connect');
-      socket.on('name', function(username) {
-        console.log('recieved a msg on client: ' + username.username );
-        if (username.username == 'finished') {
-          $scope.searching = false;
-        } else {
-          $scope.hits.push(username.username);
-        }
-        $scope.$digest();
-      });
+  function onConnect() {
+    socket.on('name', function(username) {
+      if (username.username == 'finished') {
+        $scope.searching = false;
+      } else {
+        $scope.hits.push(username.username);
+      }
+      $scope.$digest();
     });
+  }
 }]);
 
-angular.module('app').factory('socket', [ function() {
+angular.module('app').factory('socket', [
+function() {
+
   var socket = {};
-
   socket._socket = io();
+  socket.emit = emit;
+  socket.on = on;
 
-  socket.emit = function(_event, data) {
-    console.log('socket emitting on' + _event);
+  function emit(_event, data) {
     socket._socket.emit(_event, data);
   }
 
-  socket.on = function(_event, func) {
-      return socket._socket.on(_event, func);
+  function on(_event, func) {
+    return socket._socket.on(_event, func);
   }
 
   return socket;
 }]);
 
-angular.module('app').factory('search', [ 'urls', '$http', function(urls, $http) {
+angular.module('app').factory('search', [ 'urls', '$http',
+function(urls, $http) {
+
   search = {};
   search.hits = [];
+  search.whois = whois;
 
-  search.whois = function(name) {
+  function whois(name) {
     search.hits = [];
     for (url in urls) {
       $http.get(urls[url].api, { params: { user: name } }).then( function(response) {
         if (response.data == false) {
           search.hits.push(urls[url].page + name);
         }
-      })
+      });
     }
   };
 
