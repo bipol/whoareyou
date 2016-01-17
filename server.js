3b98f71c716e01c320473b92b1250c8e1f5773be
Bipol Alam <bipolalam@gmail.com>
2016-01-17 10:04:26 -0600

    refactored code to be a bit prettier and added a urls.json file

--- server.js
+++ server.js
@@ -1,105 +1,50 @@
+
 var express = require('express');
 var cors = require('cors');
 var q = require('q');
 var request = require('request');
+var urls = require('./urls.json')
+
 var app = express();
+var onConnection = onConnection;
+var queryUrl = queryUrl;
+
+//create server
+var server = app.listen(process.env.PORT || 8000, function () {
+	var host = "127.0.0.1";
+	var port = server.address().port;
+	console.log('whoareyou listening at http://%s:%s', host, port);
+});
 
 //static
 app.use(express.static(__dirname + "/src/"));
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 app.use(cors());
 
-//create server
-var server = app.listen(process.env.PORT || 8000, function () {
-	var host = "127.0.0.1";
-	var port = server.address().port;
-
-	console.log('Example app listening at http://%s:%s', host, port);
-});
-
 app.io = require('socket.io')(server);
-
-app.io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-      console.log('user disconnected');
-  });
-});
-
-app.io.on('connection', function(socket) {
-
-  socket.on('name', function(data) {
-    console.log('name' + data);
-  });
-
-  socket.on('usernames', function(data) {
-    var name = data.name;
-
-    console.log('received message on usernames ' + data.name);
-    var promises = [];
-  	for (site in urls) {
-  		promises.push(query_url(urls[site], name, socket));
-  	}
-    q.all(promises).then(function() {
-			socket.emit('name', {'username': 'finished'});
-		});
-  });
-});
-
-urls = {
-  "reddit":  {
-		url: "https://www.reddit.com/user/",
-		protocol: 'https',
-	},
-  "twitter":  {
-		url: "https://www.twitter.com/",
-		protocol: 'https',
-	},
-  "instagram":  {
-		url: "https://www.instagram.com/",
-		protocol: 'https',
-	},
-  "youtube":  {
-		url: "https://www.youtube.com/",
-		protocol: 'https',
-	},
-  "imgur": {
-		url: "https://www.imgur.com/",
-		protocol: 'https',
-	},
-  "pornhub": {
-		url: "http://www.pornhub.com/users/",
-		protocol: 'http',
-	},
-  "xvideos": {
-		url: "http://www.xvideos.com/profiles/",
-		protocol: 'http',
-	},
-  "xhamster": {
-		url: "http://xhamster.com/user/",
-		protocol: 'http',
-	},
-  "pinterest": {
-		url: "https://www.pinterest.com/",
-		protocol: 'https',
-	},
-  "google+": {
-		url: "https://plus.google.com/+",
-		protocol: 'https',
-	},
-  "flickr": {
-		url: "https://www.flickr.com/photos/",
-		protocol: 'https',
-	},
-}
+app.io.on('connection', onConnection);
 
 //routes
 app.get('/', function(req, res) {
   res.sendFile('./src/index.html');
 });
 
-// function that returns promises to handle the urls
-var query_url = function(site, name, socket) {
+function onConnection(socket) {
+  socket.on('usernames', function(data) {
+    var name = data.name;
+
+    console.log('received message on usernames ' + data.name);
+    var promises = [];
+  	for (site in urls) {
+  		promises.push(queryUrl(urls[site], name, socket));
+  	}
+    q.all(promises).then(function() {
+			socket.emit('name', {'username': 'finished'});
+		});
+  });
+}
+
+function queryUrl(site, name, socket) {
   var deferred = q.defer();
 	console.log(site);
   request(site.url + name, {timeout: 2000}, function(error, response, body) {
@@ -112,4 +57,4 @@ var query_url = function(site, name, socket) {
   	}
   });
   return deferred.promise;
-};
+}
